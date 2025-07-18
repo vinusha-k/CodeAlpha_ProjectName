3cd17480e8b82f2f4a9c5724bec60809c25b0e03
vinusha-k <vinusha1210@gmail.com>
2025-07-18 19:14:09 +0530

    Create chatbot.py

---
+++ chatbot.py
@@ -0,0 +1,16 @@
+def chatbot():
+    print("Chatbot: Hi! Type 'bye' to end the chat.")
+    while True:
+        user_input = input("You: ").lower()
+
+        if user_input == "hello":
+            print("Chatbot: Hi!")
+        elif user_input == "how are you":
+            print("Chatbot: I'm fine, thanks!")
+        elif user_input == "bye":
+            print("Chatbot: Goodbye!")
+            break
+        else:
+            print("Chatbot: Sorry, I didn't understand that.")
+
+chatbot()
